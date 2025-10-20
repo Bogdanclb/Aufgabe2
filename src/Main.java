5b41421e870e17b19c5184bc1a38006f97081dd4
bogdan <bogdan.calburean2004@gmail.com>
2025-10-20 21:11:16 +0300

    2.3,2.4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 void main() {
     Zahl z = new Zahl();
-    int [] Zahlen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int [] Zahlen = {4,8,3,10,17};
     System.out.println(z.MaxZahl(Zahlen));
     System.out.println(z.MinZahl(Zahlen));
+    System.out.println(z.MaxSumme(Zahlen));
+    System.out.println(z.MinSumme(Zahlen));
     }
--- src/Zahl.java
+++ src/Zahl.java
@@ -19,4 +19,34 @@ public class Zahl {
         return min;
     }
 
+    public int MaxSumme(int [] Zahlen){
+        int Summe = 0;
+        for (int i = 0; i < Zahlen.length; i++){
+            Summe += Zahlen[i];
+        }
+        int MaxSumme = -99999;
+        for (int i = 0; i < Zahlen.length; i++){
+            int x = Summe;
+            x = x - Zahlen[i];
+            if (x > MaxSumme)
+                MaxSumme = x;
+        }
+        return MaxSumme;
+    }
+
+    public int MinSumme(int [] Zahlen){
+        int Summe = 0;
+        for (int i = 0; i < Zahlen.length; i++){
+            Summe += Zahlen[i];
+        }
+        int MinSumme = 99999;
+        int x;
+        for (int i = 0; i < Zahlen.length; i++){
+            x = Summe;
+            x = x - Zahlen[i];
+            if (x < MinSumme)
+                MinSumme = x;
+        }
+        return MinSumme;
+    }
 }
