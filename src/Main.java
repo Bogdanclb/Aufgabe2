ad587356146699c9184e3ce4422d7667c9720dca
bogdan <bogdan.calburean2004@gmail.com>
2025-10-21 20:22:29 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 void main() {
     Zahl z = new Zahl();
     int [] Zahlen = {4,8,3,10,17};
-    System.out.println(z.MaxZahl(Zahlen));
-    System.out.println(z.MinZahl(Zahlen));
-    System.out.println(z.MaxSumme(Zahlen));
-    System.out.println(z.MinSumme(Zahlen));
+    System.out.println("Max Zahl : "+z.MaxZahl(Zahlen));
+    System.out.println("Min Zahl : " + z.MinZahl(Zahlen));
+    System.out.println("Max Summe : " + z.MaxSumme(Zahlen));
+    System.out.println("Min Summe : " + z.MinSumme(Zahlen));
     }
